bf684ef8ca806509eda71de8b1a9b78dbf176195
walker93 <walker1993@live.it>
2018-06-20 23:22:33 +0200

    secondo upload

--- BulkUnArchiver/Form1.Designer.vb
+++ BulkUnArchiver/Form1.Designer.vb
@@ -22,12 +22,27 @@ Partial Class Form1
     'Non modificarla mediante l'editor del codice.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btn_sorgente = New System.Windows.Forms.Button()
         Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btn_dest = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btn_estrai = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripProgressBar1 = New System.Windows.Forms.ToolStripProgressBar()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.stato_label = New System.Windows.Forms.ToolStripStatusLabel()
         Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.ContextMenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
@@ -43,54 +58,165 @@ Partial Class Form1
         '
         Me.GroupBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.btn_sorgente)
         Me.GroupBox1.Controls.Add(Me.Label1)
         Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(413, 41)
+        Me.GroupBox1.Size = New System.Drawing.Size(332, 41)
         Me.GroupBox1.TabIndex = 1
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Sorgente"
         '
-        'Button1
+        'btn_sorgente
         '
-        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Button1.Location = New System.Drawing.Point(332, 11)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "&Sfoglia"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btn_sorgente.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_sorgente.Location = New System.Drawing.Point(251, 11)
+        Me.btn_sorgente.Name = "btn_sorgente"
+        Me.btn_sorgente.Size = New System.Drawing.Size(75, 23)
+        Me.btn_sorgente.TabIndex = 1
+        Me.btn_sorgente.Text = "&Sfoglia"
+        Me.btn_sorgente.UseVisualStyleBackColor = True
         '
         'RichTextBox1
         '
         Me.RichTextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.RichTextBox1.Location = New System.Drawing.Point(12, 59)
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 106)
         Me.RichTextBox1.Name = "RichTextBox1"
-        Me.RichTextBox1.Size = New System.Drawing.Size(413, 226)
+        Me.RichTextBox1.Size = New System.Drawing.Size(413, 249)
         Me.RichTextBox1.TabIndex = 2
         Me.RichTextBox1.Text = ""
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox2.Controls.Add(Me.btn_dest)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 59)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(332, 41)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Destinazione"
+        '
+        'btn_dest
+        '
+        Me.btn_dest.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_dest.Location = New System.Drawing.Point(251, 11)
+        Me.btn_dest.Name = "btn_dest"
+        Me.btn_dest.Size = New System.Drawing.Size(75, 23)
+        Me.btn_dest.TabIndex = 1
+        Me.btn_dest.Text = "&Sfoglia"
+        Me.btn_dest.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Percorso..."
+        '
+        'btn_estrai
+        '
+        Me.btn_estrai.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_estrai.Location = New System.Drawing.Point(350, 70)
+        Me.btn_estrai.Name = "btn_estrai"
+        Me.btn_estrai.Size = New System.Drawing.Size(75, 23)
+        Me.btn_estrai.TabIndex = 1
+        Me.btn_estrai.Text = "&Estrai"
+        Me.btn_estrai.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripProgressBar1, Me.ToolStripStatusLabel1, Me.stato_label})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 358)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(437, 22)
+        Me.StatusStrip1.TabIndex = 3
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripProgressBar1
+        '
+        Me.ToolStripProgressBar1.Name = "ToolStripProgressBar1"
+        Me.ToolStripProgressBar1.Size = New System.Drawing.Size(100, 16)
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(37, 17)
+        Me.ToolStripStatusLabel1.Text = "Stato:"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(350, 27)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(71, 17)
+        Me.CheckBox1.TabIndex = 4
+        Me.CheckBox1.Text = "Overwrite"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(120, 26)
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(119, 22)
+        Me.ToolStripMenuItem1.Text = "&Cancella"
+        '
+        'stato_label
+        '
+        Me.stato_label.Name = "stato_label"
+        Me.stato_label.Size = New System.Drawing.Size(43, 17)
+        Me.stato_label.Text = "Pronto"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(437, 297)
+        Me.ClientSize = New System.Drawing.Size(437, 380)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.btn_estrai)
         Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
         Me.Name = "Form1"
         Me.Text = "BulkUnArchiver"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ContextMenuStrip1.ResumeLayout(False)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
     Friend WithEvents Label1 As Label
     Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btn_sorgente As Button
     Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btn_dest As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btn_estrai As Button
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripProgressBar1 As ToolStripProgressBar
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents stato_label As ToolStripStatusLabel
 End Class
--- BulkUnArchiver/Form1.vb
+++ BulkUnArchiver/Form1.vb
@@ -2,15 +2,46 @@
 
 Public Class Form1
     Public archives As New List(Of String)
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Public dest_dir As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btn_sorgente.Click
         Dim dialog = FolderBrowserDialog1.ShowDialog
         If dialog = DialogResult.OK Then
+            stato_label.Text = "Scansione degli archivi..."
+            btn_estrai.Enabled = False
             AddDirectory(FolderBrowserDialog1.SelectedPath)
+            Label1.Text = FolderBrowserDialog1.SelectedPath
+            stato_label.Text = "Pronto"
+            btn_estrai.Enabled = True
         End If
     End Sub
 
-    Public Sub unzip(path As String)
-
+    Public Sub unzip(path As String, Optional overwrite As Boolean = True)
+        Try
+            Dim p As New ProcessStartInfo()
+            p.FileName = "7za.exe"
+            p.Arguments = String.Format("x {0} -o{1} -ao{2}", path, dest_dir, If(overwrite, "a", "s"))
+            p.WindowStyle = ProcessWindowStyle.Hidden
+            Dim proc = Process.Start(p)
+            proc.WaitForExit()
+            Select Case proc.ExitCode
+                Case 0
+                    PrintLog("'" & path & "'  processato correttamente")
+                Case 1
+                    PrintLog("WARNING: '" & path & "' ha dato problemi")
+                Case 2
+                    PrintLog("FATAL: '" & path & "' Errore Fatale")
+                Case 7
+                    PrintLog("ERROR: '" & path & "' riga di comando non valida")
+                Case 8
+                    PrintLog("ERROR: '" & path & "' memoria esaurita")
+                Case 255
+                    PrintLog("ERROR: '" & path & "' Processo interrotto dall'utente")
+                Case Else
+                    PrintLog("ERRORE IMPREVISTO: '" & path & "'")
+            End Select
+        Catch ex As Exception
+            PrintLog("ERRORE: '" & path & "' estrazione non riuscita.")
+        End Try
     End Sub
 
     Public Sub PrintLog(Text As String)
@@ -20,5 +51,24 @@ Public Class Form1
         RichTextBox1.Text += rtf
     End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btn_dest.Click
+        Dim dialog = FolderBrowserDialog1.ShowDialog
+        If dialog = DialogResult.OK Then
+            dest_dir = FolderBrowserDialog1.SelectedPath
+            Label2.Text = dest_dir
+        End If
+    End Sub
 
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btn_estrai.Click
+        PrintLog("Avvio estrazione di " & archives.Count & " archivi.")
+        btn_estrai.Enabled = False
+        For Each archive In archives
+            unzip(archive, CheckBox1.Checked)
+        Next
+        btn_estrai.Enabled = True
+    End Sub
+
+    Private Sub ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem1.Click
+        RichTextBox1.Clear()
+    End Sub
 End Class
